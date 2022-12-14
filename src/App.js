6739efbdfdd34e28a9a514554d76f88581a3e599
anindya <pamsrewari@gmail.com>
2022-12-15 06:59:35 +0700

    Create home page and form page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/Home';
+import Form from './pages/Form';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Home /> */}
+      <Form />
+    </div>
+  );
+}
+
+export default App;
